2407cda1de6950cc70057ea51d84b04eff0df074
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-05-13 15:40:55 +0530

    understood web server and logging

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,7 +1,25 @@
  
  const http = require('http');
+ const fs = require('fs'); // to create log of the incoming requests
+
 
  const myServer = http.createServer((req, res) => {
-   console.log("New request received");
-   res.end("Hello from the server!");
- }); //this creates a server object
+   //console.log("New request received");
+   //console.log(req.headers);
+
+   const log = `${Date.now()}: ${req.url} New request received \n`;
+   fs.appendFile('log.txt', log, (err, data) =>{
+       res.end("Hello from the server again!");
+       switch(req.url){
+        case '/': res.end("HomePage");
+        break;
+        case '/about': res.end("About us");
+        break;
+        default: res.end("404: Page not found");
+       }
+
+   });
+
+ });//this creates a server object
+
+ myServer.listen(9000, () => console.log("Server started"));//this tells the server to listen on port 8000
