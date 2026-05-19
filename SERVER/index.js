562a50b2552c23bc553ec2aeb4aabf1a24d04428
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-05-19 18:36:15 +0530

    Learning express framework

--- SERVER/index.js
+++ SERVER/index.js
@@ -9,9 +9,9 @@ const url = require('url'); // to parse the incoming request url
    //console.log(req.headers);
    if(req.url === '/favicon.ico') return res.end(); // to ignore the favicon request  
 
-   const log = `${Date.now()}: ${req.url} New request received \n`;
+   const log = `${Date.now()}: ${req.method} ${req.url} New request received \n`;
    const myUrl = url.parse(req.url, true); // to parse the incoming request url
-   console.log(myUrl);
+   
 
    fs.appendFile('log.txt', log, (err, data) =>{
       // res.end("Hello from the server again!");
@@ -19,13 +19,28 @@ const url = require('url'); // to parse the incoming request url
        switch(myUrl.pathname){
 
         case "/": 
-        res.end("HomePage");
+        if(req.method === "GET"){
+        res.end("HomePage");}
         break;
 
         case "/about":
           const username = myUrl.query.username || "Subhava Ojha"; // to get the username from the query string
            res.end(`Hi, ${username}! Welcome to the about page!`);
         break;
+
+        case "/contact":
+          res.end("Get lost! Just kidding, contact us at contact@example.com");
+          break;
+
+        case "/signup":
+          if(req.method ==="GET") res.end("This is a Signup page");
+          else if(req.method === "POST"
+            
+          ){
+            //DB query
+            res.end("Signup successful!");
+        }
+          break;
 
         default:
            res.end("404: Page not found");
