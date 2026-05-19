cbfb1860ba960f365d2d78c54af17e4e54b66838
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-05-19 18:06:40 +0530

    till http server

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,25 +1,38 @@
  
  const http = require('http');
  const fs = require('fs'); // to create log of the incoming requests
+const url = require('url'); // to parse the incoming request url
 
 
  const myServer = http.createServer((req, res) => {
    //console.log("New request received");
    //console.log(req.headers);
+   if(req.url === '/favicon.ico') return res.end(); // to ignore the favicon request  
 
    const log = `${Date.now()}: ${req.url} New request received \n`;
+   const myUrl = url.parse(req.url, true); // to parse the incoming request url
+   console.log(myUrl);
+
    fs.appendFile('log.txt', log, (err, data) =>{
-       res.end("Hello from the server again!");
-       switch(req.url){
-        case '/': res.end("HomePage");
+      // res.end("Hello from the server again!");
+       
+       switch(myUrl.pathname){
+
+        case "/": 
+        res.end("HomePage");
         break;
-        case '/about': res.end("About us");
+
+        case "/about":
+          const username = myUrl.query.username || "Subhava Ojha"; // to get the username from the query string
+           res.end(`Hi, ${username}! Welcome to the about page!`);
         break;
-        default: res.end("404: Page not found");
+
+        default:
+           res.end("404: Page not found");
        }
 
    });
 
  });//this creates a server object
 
- myServer.listen(9000, () => console.log("Server started"));//this tells the server to listen on port 8000
+ myServer.listen(8000, () => console.log("Server started"));//this tells the server to listen on port 8000
